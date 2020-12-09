09e83860b3a99f3a37af04cc87374e494aaac993
mikeroswell <mike.roswell@gmail.com>
2020-12-09 10:18:33 -0500

    typos causing errors

--- R/multBinom.R
+++ R/multBinom.R
@@ -27,14 +27,14 @@
 #' mydat <- rbinom(n_samples, n_trials, p_true)
 #' 
 #' # run tests
-#' multBinom(mydat, ptrue, n_trials, testv = "binom.test")
-#' multBinom(mydat, ptrue, n_trials, testv = "wald")
+#' multBinom(mydat, p_true, n_trials, testv = "binom.test")
+#' multBinom(mydat, p_true, n_trials, testv = "wald")
 #' 
 #' @export 
 
 multBinom <- function(dat, prob0, n, testv = c("binom.test", "accept", "chisq", "wald")) {
 	if (testv == "binom.test") {
-		df <- map_dfr(dat, function(d) {
+		df <- purrr::map_dfr(dat, function(d) {
 			bt <- binom.test(d, n, p = prob0, alternative = "less")
 			gt <- binom.test(d, n, p = prob0, alternative = "greater")
 			cp <- bt$p.value
@@ -49,7 +49,7 @@ multBinom <- function(dat, prob0, n, testv = c("binom.test", "accept", "chisq", 
 		})
 	}
 	if (testv == "accept") {
-		df <- map_dfr(dat, function(d) {
+		df <- purrr::map_dfr(dat, function(d) {
 			p <- acceptbin(d, n, p = prob0)
 			ci <- acceptinterval(d, n)
 			return(data.frame(est = d/n
@@ -60,7 +60,7 @@ multBinom <- function(dat, prob0, n, testv = c("binom.test", "accept", "chisq", 
 		})
 	}
 	if (testv == "wald") {
-		df <- future_map_dfr(dat, function(d) {
+		df <- purrr::map_dfr(dat, function(d) {
 			p <- pnorm((d/n - prob0)/(((d/n) * ((n - d)/n))^0.5 * n^(-0.5)))
 			ci <- prop.test(d, n, p = prob0, alternative = "two.sided")
 			return(data.frame(est = d/n
