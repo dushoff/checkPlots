b8dbf7dc78388c7154eccad110e7bb9e8f256878
mikeroswell <mike.roswell@gmail.com>
2024-05-08 15:03:42 -0400

    Minor copyediting since we're sharing this package again.

--- R/rangePlot.R
+++ R/rangePlot.R
@@ -6,28 +6,32 @@
 #' (true) value of the statistic to examine statistical coverage and bias of CI
 #' 
 #' @template tf_template
-#' @param target scalar, value of statistic under (true) hypothesis
+#' @param target scalar, value of statistic under (true) hypothesis, or
+#'   "sampleMean", default, which calculates the sample mean.
 #' @param orderFun one of \code{c(slug, milli, blob)}, determining the way CI
 #' are arranged in the plot
 #' @param conf scalar in \[0,1\], nominal confidence level of simulated CI
 #' @param opacity scalar value for \code{alpha}
 #' @param fatten scalar passed to \code{geom_pointrange}
 #' @param title character vector for ggtitle
-#' @param targ_num desired number of lines on graph, for dynamic data thinning
+#' @param targNum desired number of lines on graph, for dynamic data thinning
 #'  
 #' @importFrom stats median quantile runif pnorm pbinom rbinom rnorm 
 #' @return a ggplot object with simulated CI arranged about their target value
 #'  
 #' @export 
 rangePlot <- function(tf
-                      , target = mean(tf$est)
+                      , target = "sampleMean"
                       , orderFun = slug
-                      , conf=0.95
+                      , conf = 0.95
                       , opacity = 0.2
                       , fatten = 0.1
                       , title = "Range plot"
-                      , targ_num = 1e3){
-  thinner <- max(floor(length(tf$est)/targ_num), 1)
+                      , targNum = 1e3){
+  if(target == "sampleMean"){
+    target <- mean(tf$est)
+  }
+  thinner <- max(floor(length(tf$est)/targNum), 1)
   thinned <- tf[seq(thinner
                     , length(tf$est)
                     , thinner)
@@ -38,7 +42,8 @@ rangePlot <- function(tf
     , aes(x = quantile, y = est, ymin = lower, ymax = upper)
     )
     + geom_pointrange(alpha = opacity, fatten = fatten
-                      , aes(color = ifelse(lower > target | upper < target, "red", "grey")))
+                      , aes(color = ifelse(lower > target | upper < target
+                                           , "red", "grey")))
     + geom_hline(yintercept = target, color = "blue")
     + geom_vline(
       xintercept = c((1 - conf)/2, 1 - (1 - conf)/2)
@@ -48,7 +53,8 @@ rangePlot <- function(tf
     + ylab("estimate")
     + ggtitle(title)
     + scale_x_continuous(expand = c(0,0)
-                         , breaks = c((1 - conf)/2, 0.25, 0.5, 0.75, conf + (1 - conf)/2)
+                         , breaks = c((1 - conf)/2, 0.25, 0.5, 0.75
+                                      , conf + (1 - conf)/2)
                          , labels = function(breaks){signif(breaks, 3)})
     + scale_color_manual(values = c("grey", "red"))
     + guides(color = "none")
